c322f1e740de128635bf769c903604d5514869a3
zidney06 <zidniimportant@gmail.com>
2025-06-26 14:36:47 +0700

    membuat UI untuk page login, dashoard

---
+++ frontend/src/pages/Biodata.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default function Biodata() {
+  return (
+    <>
+      <Navbar />
+      <div className="container-fluid p-0 red"></div>
+    </>
+  );
+}
